254e34c32d535767cad97b2f16b2ca3ab4f4d482
npc-123 <aziznasrul85@gmail.com>
2024-02-17 15:25:36 +0700

    app: tambah sweetalert

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,6 +6,7 @@
 
         <title>{{ $title ?? 'Page Title' }}</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         @yield('style')
     </head>
     <body>

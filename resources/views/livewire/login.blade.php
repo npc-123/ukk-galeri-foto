7b478d4614348aa49d489cf7435a01ade4cb610d
npc-123 <aziznasrul85@gmail.com>
2024-02-17 09:32:51 +0700

    ganti css login & register

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login - Galeri Foto</title>
-    <link rel="stylesheet" href="/css/login.css">
+    <link rel="stylesheet" href="/css/login-register.css">
   </head>
   <body>
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">

f21fadd6b05715de547f3c91b8a8428ac6d990ea
npc-123 <aziznasrul85@gmail.com>
2024-02-20 16:31:09 +0700

    Fitur Settings sudah selesai

--- app/Livewire/Settings.php
+++ app/Livewire/Settings.php
@@ -2,10 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Settings extends Component
 {
+    use WithFileUploads;
+
+    public $photo;
+    public $imageUpload;
     public $name;
     public $username;
     public $email;
@@ -13,15 +20,76 @@ class Settings extends Component
 
     public function render()
     {
-        return view('livewire.settings');
+        return view('livewire.settings',[
+            'image' => auth()->user()->Foto
+        ]);
     }
     public function mount(){
         $this->name = auth()->user()->NamaLengkap;
         $this->username = auth()->user()->username;
         $this->email = auth()->user()->email;
         $this->address = auth()->user()->Alamat;
+        $this->photo = auth()->user()->Foto;
     }
     public function resetInput(){
-        $this->mount();
+        $this->mount([
+            $this->imageUpload = null,
+        ]);
     }
+    public function updated($name, $value){
+        $this->resetValidation($name);
+        $this->resetErrorBag($name);
+    }
+    public function update(){
+        $user = User::where('UserID', auth()->user()->UserID)->first();
+        $validated = $this->validate([
+            'imageUpload' => 'nullable|image|max:2024',
+            'name' => 'required',
+            'username' => [
+                'required',
+                'min:5',
+                Rule::unique('users')->ignore($user->UserID, 'UserID'),
+            ],
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users')->ignore($user->UserID, 'UserID'),
+            ],
+            'address' => 'required',
+        ],[
+            'name.required' => 'Nama Tidak boleh Kosong',
+            'username.required' => 'Username tidak boleh Kosong',
+            'username.min' => 'Username minimal 5 karakter',
+            'username.unique' => 'Username ini sudah digunakan',
+            'email.required' => 'Email tidak boleh Kosong',
+            'email.email' => 'Masukkan Email yang benar',
+            'email.unique' => 'Email ini sudah digunakan',
+            'address.required' => 'Alamat tidak boleh Kosong',
+        ]);
+        if ($user) {
+            $data = [
+                'NamaLengkap' => $this->name,
+                'username' => $this->username,
+                'email' => $this->email,
+                'Alamat' => $this->address,
+                'Foto' => $this->photo,
+            ];
+            if ($this->imageUpload && $this->imageUpload->isValid()) {
+                if (file_exists(public_path('storage/' . $user->Foto))) {
+                    unlink(public_path('storage/' . $user->Foto));
+                }
+                $newImagePath = $this->imageUpload->store('image/user/'. $user->UserID . '/profile', 'public');
+                $imagePath = storage_path('app/public/' . $newImagePath);
+    
+                if (file_exists($imagePath)) {
+                    $data['Foto'] = $newImagePath;
+                }
+            }
+            if ($user->update($data)) {
+                $this->dispatch('updateSuccess', type: 'success', title: 'Operasi Berhasil', message: 'Berhasil memperbarui pengaturan akun');
+            } else {
+                $this->dispatch('updateFailed', type: 'error', title: 'Ada yang salah', message: 'Gagal memperbarui pengaturan akun');
+            }
+        }
+    }    
 }
--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -4,17 +4,20 @@
             <form action="" method="post" class="mt-11 w-full sm:w-[75%] md:w-[40%] lg:w-[30%] p-[10px]">
                 <!-- @csrf -->
                 <div id="photo-profile" class="flex justify-start">
-                    <img src="https://miro.medium.com/v2/resize:fit:698/1*0jjdu52m0MO4SjLWiCVOlg.jpeg"
-                        alt="phoro profile" class="w-32 object-contain rounded-full">
+                    @if ($imageUpload)
+                    <img src="{{$imageUpload->temporaryUrl()}}" alt="photo profile" class="w-32 h-32 object-cover rounded-full">
+                    @else
+                    <img src="/storage/{{$image}}" alt="photo profile" class="w-32 h-32 object-cover rounded-full">
+                    @endif
+                    
                     <div class="flex items-center px-6">
                         <label for="upload"
                             class="cursor-pointer inline-block py-2 px-4 bg-blue-500 text-white rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
                             tabindex="0">
                             <span class="d-none d-sm-block">Ganti</span>
-                            <i class="bx bx-upload d-block d-sm-none"></i>
-                            <input wire:model="imageUpload" name="image" type="file" id="upload" class="hidden"
-                                accept="image/png">
+                            <input wire:model="imageUpload" name="imageUpload" type="file" id="upload" class="hidden">
                         </label>
+                        @error('imageUpload') <div class="text-red-500 text-sm">{{ $message }}</div> @enderror
                     </div>
                 </div>
                 <div class="block mt-7">
@@ -22,33 +25,37 @@
                         <label for="name" class="mt-3 block text-sm font-medium leading-6 text-gray-900">Nama
                             Lengkap</label>
                         <div class="mt-2">
-                            <input wire:model="name" id="name" name="name" type="text" autocomplete="off" required
-                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none">
+                            <input wire:model="name" @error('name') wire:model.live="name" @enderror id="name" name="name" type="text" autocomplete="off" required
+                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600  sm:text-sm sm:leading-6 pl-3 focus:outline-none @error('name') ring-red-500 focus:ring-red-600 @enderror">
+                                <div class="text-red-500 text-sm">@error('name') {{ $message }} @enderror</div>
                         </div>
                     </div>
                     <div class="w-full">
                         <label for="username"
                             class="mt-3 block text-sm font-medium leading-6 text-gray-900">Username</label>
                         <div class="mt-2">
-                            <input wire:model="username" id="username" name="username" type="text" autocomplete="off"
+                            <input wire:model="username" @error('username') wire:model.live="username" @enderror id="username" name="username" type="text" autocomplete="off"
                                 required
-                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none">
+                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none @error('username') ring-red-500 focus:ring-red-600 @enderror">
+                                <div class="text-red-500 text-sm">@error('username') {{ $message }} @enderror</div>
                         </div>
                     </div>
                     <div class="w-full">
                         <label for="email" class="mt-3 block text-sm font-medium leading-6 text-gray-900">Email</label>
                         <div class="mt-2">
-                            <input wire:model="email" id="email" name="email" type="text" autocomplete="off" required
-                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none">
+                            <input wire:model="email" @error('email') wire:model.live="email" @enderror id="email" name="email" type="text" autocomplete="off" required
+                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none @error('email') ring-red-500 focus:ring-red-600 @enderror">
+                                <div class="text-red-500 text-sm">@error('email') {{ $message }} @enderror</div>
                         </div>
                     </div>
                     <div class="w-full">
                         <label for="address"
                             class="mt-3 block text-sm font-medium leading-6 text-gray-900">Alamat</label>
                         <div class="mt-2">
-                            <textarea wire:model="address" id="address" name="address" rows="4" autocomplete="off"
+                            <textarea wire:model="address" @error('address') wire:model.live="address" @enderror id="address" name="address" rows="4" autocomplete="off"
                                 required
-                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none resize-none"></textarea>
+                                class="block w-full rounded-xl border-0 py-1.5 text-gray-900 shadow-sm ring-2 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 pl-3 focus:outline-none resize-none @error('address') ring-red-500 focus:ring-red-600 @enderror"></textarea>
+                                <div class="text-red-500 text-sm">@error('address') {{ $message }} @enderror</div>
                         </div>
                     </div>
                 </div>
@@ -58,15 +65,21 @@
                         class="cursor-pointer mr-3 py-2 px-4 bg-red-500 text-white rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 focus:ring-offset-gray-100">
                         Atur Ulang
                     </span>
-                    <input type="submit" value="Simpan"
+                    {{-- <input type="submit" value="Simpan"
+                        class="cursor-pointer inline-block py-2 px-4 bg-blue-500 text-white rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"> --}}
+                    <span id="simpan"
                         class="cursor-pointer inline-block py-2 px-4 bg-blue-500 text-white rounded-lg shadow-lg transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                        Simpan
+                    </span>
                 </div>
             </form>
         </div>
     </div>
-    @section('script')
+    @script
     <script>
         const btnReset = document.getElementById('reset');
+        const btnSimpan = document.getElementById('simpan');
+
         btnReset.addEventListener('click', function () {
             Swal.fire({
                 title: "Apa anda yakin?",
@@ -80,11 +93,48 @@
                 reverseButtons: true
             }).then((result) => {
                 if (result.isConfirmed) {
-                    @this.call('resetInput');
+                    @this.call('resetInput');                          
                 }
             });
-        })
+        });
 
-    </script>
-    @endsection
+        btnSimpan.addEventListener('click', function () {
+            Swal.fire({
+                title: 'Konfirmasi Perubahan',
+                text: "Semua Perubahan Akan Disimpan!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3b82f6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'OK',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    @this.call('update');
+                }
+            });
+        });
+        </script>
+        <script>
+            Livewire.on('updateSuccess', function(data) {
+                Swal.fire({
+                    icon: data.type,
+                    title: data.message,
+                    confirmButtonColor: '#3b82f6',
+                    timer: 3000,
+                    timerProgressBar: true,
+                    showConfirmButton: false
+                    // text: data.message
+                });
+            });
+            Livewire.on('updateFailed', function(data) {
+                Swal.fire({
+                    icon: data.type,
+                    title: data.title,
+                    confirmButtonColor: '#3b82f6',
+                    text: data.message
+                });
+            });
+        </script>    
+    @endscript
 </div>

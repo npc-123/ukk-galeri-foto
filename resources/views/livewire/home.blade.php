52164cf82536067016169e9028cd46acb24372f7
npc-123 <aziznasrul85@gmail.com>
2024-02-20 15:40:56 +0700

    home: hapus extends main

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,9 +1,7 @@
 <div>
-    @extends('layouts.main')
-    @section('content')
     <h1 class="text-3xl font-bold underline"> Hello world! </h1>
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
         Hello
     </button>
-    @endsection
+    <input type="text" name="" wire:model="username" id="">
 </div>

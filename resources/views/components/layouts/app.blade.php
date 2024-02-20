deb5ca8da27291539d16cc8cffc06675fd9cb5f9
npc-123 <aziznasrul85@gmail.com>
2024-02-20 16:29:57 +0700

    app: tambah boxicons

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <title>{{ $title ?? 'Page Title' }}</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         @yield('style')
     </head>
     <body>

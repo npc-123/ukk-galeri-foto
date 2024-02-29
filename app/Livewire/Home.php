05e0ddda9703dba88623c7440e992b9d6ae7248a
npc-123 <aziznasrul85@gmail.com>
2024-02-29 15:22:11 +0700

    Fitur Home sudah selesai

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,19 @@
 
 namespace App\Livewire;
 
+use App\Models\Foto;
 use Livewire\Component;
 
 class Home extends Component
 {
+    public $on_page = 30;
     public function render()
     {
-        return view('livewire.home');
+        return view('livewire.home', [
+            'posts' => Foto::latest()->take($this->on_page)->get(),
+        ]);
+    }
+    public function loadMore(){
+        $this->on_page += 20;
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,37 @@
 <div>
-    <h1 class="text-3xl font-bold underline"> Hello world! </h1>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4">
-        Hello
-    </button>
-    <input type="text" name="" wire:model="username" id="">
+  <x-slot name="title">Beranda</x-slot>
+    @section('style')
+    <style>
+        .img-container{
+          column-count: auto;
+          column-width: 14rem;
+          column-gap: 0.5rem;
+          padding: 1rem;
+        }
+        
+        .img-result {
+          margin-bottom: 0.5rem;
+          display: block;
+        }
+        .img-result img {
+          height: 100%;
+          width: 100%;
+          object-fit: cover;
+          border-radius:  0.2rem;
+        }
+    </style>
+    @endsection
+    <div class="img-container">
+        @foreach ($posts as $post)
+        <a class="img-result" href="{{ $post->slug }}">
+            <img loading="lazy" src="/storage/{{ $post->LokasiFile }}">
+        </a>
+        @endforeach
+        <div x-intersect.full="$wire.loadMore()" class="p-4">
+            <div wire:loading wire:target="loadMore" 
+                  class="loading-indicator">
+                  <i class='bx bx-loader-alt bx-spin' style='color:#000000' ></i> Memuat Lebih banyak postingan...  
+            </div>
+          </div>
+    </div>
 </div>

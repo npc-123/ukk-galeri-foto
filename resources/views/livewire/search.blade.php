a60af75509d85401c289fd1b8081f6c29a323d70
npc-123 <aziznasrul85@gmail.com>
2024-03-25 08:01:41 +0700

    search: tambah wire:navigate

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="flex flex-col items-center">
         @forelse ($users as $user)
-        <a href="/{{$user->username}}" class="max-w-full w-[30rem] mt-4 hover:bg-gray-200 hover:rounded-full p-4">
+        <a wire:navigate href="/{{$user->username}}" class="max-w-full w-[30rem] mt-4 hover:bg-gray-200 hover:rounded-full p-4">
             <div class="flex justify-between relative items-center">
                 <div class="flex-shrink-0">
                     <img class="w-[50px] h-[50px] object-cover rounded-full" src="{{$user->Foto}}" alt="">

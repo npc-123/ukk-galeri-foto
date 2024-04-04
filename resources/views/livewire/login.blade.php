f1e95cd2ffbbbd2f9ffa674bc6af998ed5d8d8ee
npc-123 <aziznasrul85@gmail.com>
2024-04-04 21:37:05 +0700

    login: pesan swal ganti ke toast

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -49,24 +49,40 @@
   </html>
   @if (session()->has('successCreateAccount'))
     <script>
-      Swal.fire({
-        icon: 'success',
-        title: 'Berhasil membuat Akun',
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
         showConfirmButton: false,
         timer: 3000,
-        timerProgressBar: true
-      })
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "success",
+        title: "Berhasil Membuat Akun"
+      });
     </script>
   @endif
   @if (session()->has('errorLogin'))
     <script>
-      Swal.fire({
-        icon: 'error',
-        title: 'Username atau Password Salah',
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
         showConfirmButton: false,
         timer: 3000,
-        timerProgressBar: true
-      })
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        }
+      });
+      Toast.fire({
+        icon: "error",
+        title: "Gagal Login<br>Username / Password Salah"
+      });
     </script>
   @endif
 </div>
